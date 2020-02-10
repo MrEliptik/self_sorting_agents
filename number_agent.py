41fee8cf19069bab80a942d7f09f1efb6d02a669
Victor <victormeunier.dev@gmail.com>
2020-02-10 19:36:20 +0100

    move main to number_agent.py

--- number_agent.py
+++ number_agent.py
@@ -1,10 +1,34 @@
-from agent import Agent
 from random import randrange
 import numpy as np
 import time
+import sys
 
-AGENT_NB = 50
-TICKS_SEC = 60
+class Agent:
+    def __init__(self, value, position):
+        self.value = value
+        self.position = position
+
+    def __str__(self):
+        return 'Value: {} Pos: {}'.format(self.value, self.position)
+
+    # neighbors[0] left neighbor
+    # neighbor[1] right neighbor
+    def update(self, neighbors):
+        # Check left first, because we want
+        # to sort in ascending order
+        # -1 means there's no neighbor
+        if neighbors[0] != None:
+            if neighbors[0].value > self.value:
+                # swap position
+                neighbors[0].position, self.position = self.position, neighbors[0].position
+        elif neighbors[1] != None:
+            if neighbors[1].value < self.value:
+                # swap position
+                neighbors[1].position, self.position = self.position, neighbors[1].position
+
+
+AGENT_NB = 30
+TICKS_SEC = 10
 
 def main():
     ticks = 0
@@ -22,11 +46,13 @@ def main():
         agents[positions[i]] = Agent(i, positions[i])
         wanted_result[i] = i
 
-    print_agents(agents)
+    print_agents_values(agents)
 
     # Infinite loop
     try:
         while True:
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[F")
             print("###### TICK: {} ######".format(ticks))
             # Loop through agents to update
             for (i, agent) in enumerate(agents):
@@ -49,9 +75,9 @@ def main():
                 elif neigbors[1] != None:
                     agents[neigbors[1].position] = neigbors[1]
 
-            print_agents(agents)
+            #print_agents(agents, False)
 
-            print_agents_values(agents)
+            print_agents_values(agents, False)
 
             # Check if values are correctly organized
             if np.array_equal([agent.value for agent in agents], wanted_result):
@@ -64,13 +90,19 @@ def main():
     except KeyboardInterrupt:
         print('interrupted!')
 
-def print_agents(agents):
+def print_agents(agents, dynamically=False):
+    if dynamically:
+        print([str(agent) for agent in agents], end="\r")
     print([str(agent) for agent in agents])
 
-def print_agents_values(agents):
+def print_agents_values(agents, dynamically=False):
+    if dynamically:
+        print([agent.value for agent in agents], end="\r")
     print([agent.value for agent in agents])
 
-def print_agents_positions(agents):
+def print_agents_positions(agents, dynamically=False):
+    if dynamically:
+        print([agent.position for agent in agents], end="\r")
     print([agent.position for agent in agents])
 
 if __name__ == "__main__":
